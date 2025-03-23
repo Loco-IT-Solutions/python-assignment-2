1f02d9c860be9849e0015f06fd3d5f59be799666
Loco IT Solutions <molokomodubi@outlook.com>
2025-03-23 19:04:33 +0200

    Week2 Submission

---
+++ week2.py
@@ -0,0 +1,32 @@
+# Question 1
+
+my_list = []
+
+# Question 2
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Question 3
+
+my_list.insert(1,15)
+
+# Question 4
+
+my_list.extend([50, 60, 70])
+
+# Question 5
+
+del my_list[-1]
+
+# Question 6
+
+my_list.sort()
+
+# Question 7
+
+Value30 = my_list.index(30)
+
+print(Value30)
